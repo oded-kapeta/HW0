9cf3cea4815c4d3ea4d2404e8959ee9c17fc7ab3
oded-kapeta <oded.kapeta@campus.technion.ac.il>
2023-04-24 12:04:25 +0300

    REMOVE '2'

--- src/HW0_practice.java
+++ src/HW0_practice.java
@@ -3,25 +3,25 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 public class HW0_practice {
-    public static Scanner scanner2;
-    public static void battleshipGame2() {
+    public static Scanner scanner;
+    public static void battleshipGame() {
         // TODO: Add your code here (and add more methods).
         String boardSize = new String();
-        boardSize = getBoardSize2(boardSize);
+        boardSize = getBoardSize(boardSize);
         int[] sizeOfBoard = new int[2];
         splitBoard(sizeOfBoard,boardSize);
         String battleships = new String();
-        battleships  = getBattleships2(battleships);
+        battleships  = getBattleships(battleships);
         String[] battleships_Array = battleships.split(" ");
         int[][] battleships_Length = new int[battleships_Array.length][2];
-        splitBattleships2(battleships_Length,battleships_Array);
-        print_all_Ships2(battleships_Length);
+        splitBattleships(battleships_Length,battleships_Array);
+        print_all_Ships(battleships_Length);
 
     }
 
-    public static String getBoardSize2(String boardSize) {
+    public static String getBoardSize(String boardSize) {
         System.out.println("Enter the board size");
-        boardSize = scanner2.nextLine();
+        boardSize = scanner.nextLine();
         return  boardSize;
     }
     public static void splitBoard(int[]arr, String boardSize){
@@ -30,12 +30,12 @@ public class HW0_practice {
         arr[1] = Integer.parseInt(temp[1]);
     }
 
-    public static String getBattleships2(String battleships) {
+    public static String getBattleships(String battleships) {
         System.out.println("Enter the battleships size :");
-        battleships = scanner2.nextLine();
+        battleships = scanner.nextLine();
         return battleships;
     }
-    public static void splitBattleships2(int arr[][],String arr2[]){
+    public static void splitBattleships(int arr[][],String arr2[]){
         for (int i = 0 ; i < arr2.length; i++){
             String [] tempArray = arr2[i].split("X");
             int row = Integer.parseInt(tempArray[0]);
@@ -44,7 +44,7 @@ public class HW0_practice {
             arr[i][1] = col;
         }
     }
-    public static void print_all_Ships2(int arr[][]){
+    public static void print_all_Ships(int arr[][]){
         for (int i = 0 ; i < arr.length;i++){
             System.out.println();
             for (int j = 0 ;j < arr[0].length;j++){
@@ -53,8 +53,8 @@ public class HW0_practice {
         }
     }
     public static void main(String[] args) {
-        scanner2 = new Scanner(System.in);
-        battleshipGame2();
+        scanner = new Scanner(System.in);
+        battleshipGame();
 
     }
 }
